657b5370f3febc6e608d04a2202033ef297a3f79
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2023-06-01 14:31:43 +0300

    complite general task

--- middleweres/index.js
+++ middleweres/index.js
@@ -1,9 +1,11 @@
 const validateBody = require('./validateBody');
 const isValidId = require('./isValisId');
 const authenticate = require('./authenticate');
+const upload = require('./upload');
 
 module.exports = {
   validateBody,
   isValidId,
   authenticate,
+  upload,
 };
